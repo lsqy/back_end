45d51c0ee904948f4c145d2a9a5612c48e5a5701
lsqy <lsqy93@gmail.com>
2017-03-05 23:29:45 +0800

    由diskdb迁移到mongo

--- index.js
+++ index.js
@@ -6,20 +6,48 @@ const db = require('diskdb');
 
 app.use(express.static('public'));
 
-db.connect('db',['articles']);
+db.connect('db',['oilRecord']);
 
-let article = {
-    title : 'diskDB rocks',
-    published : 'today',
-    rating : '5 stars'
-}
+let item = {
+            "id": "f352ce73e487992b41803ac23fd84f73",
+            "createtime": "2017-03-01 12:05",
+            "creator": "lsqy",
+            "oil_num": "12.27",
+            "oil_weight": "6.70 公斤",
+            "oil_density": "0.8800",
+            "oil_unit": "1",
+            "oil_instock": "2.00",
+            "isModify": "0",
+            "oilImg": "0"
+    };
 
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.header("Content-Type", "application/json;charset=utf-8");
+   // 让options请求快速返回
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  }
+  else {
+    next();
+}});
 
 app.get('/',(req,res) => {
-    console.log(11);
     res.send('hello world!');
-    //save
-    let savedArticle = db.articles.save(article);
+});
+
+
+app.get('/api/list',(req,res) => {
+    let List =  db.oilRecord.find();
+    console.log(List.length);
+    res.json(List);
+})
+
+app.get('/api/add',(req,res) => {
+    let saved = db.oilRecord.save(item);
+    res.json(saved);
 });
 
 app.listen(3009);
